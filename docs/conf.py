28b4e61c3cd35477258ac018e062011c35b3d1f4
Joshua Murray <murray.joshua@gmail.com>
2022-06-09 11:11:49 -0400

    versioning removed and config.py clean up

--- docs/conf.py
+++ docs/conf.py
@@ -99,6 +99,10 @@ graphviz_dot_args = [
     "-Efontname='%s'" % graphviz_font,
 ]
 
+html_context = {
+  "github_url":github_repo
+}
+
 diag_fontpath = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
 diag_html_image_format = "SVG"
 diag_latex_image_format = "PNG"
@@ -124,20 +128,24 @@ if found:
     spelling_ignore_importable_modules = True
     spelling_filters = []
 
+source_parsers = {
+   '.md': 'recommonmark.parser.CommonMarkParser',
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = [".rst"]
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = "index"
 
 # General information about the project.
 project = classname
-copyright = "2019, F5 Networks, Inc."
+copyright = "2022, F5 Networks, Inc."
 author = "F5 Networks, Inc."
 
 # The version info for the project you're documenting, acts as replacement for
@@ -179,8 +187,8 @@ html_theme_path = f5_sphinx_theme.get_html_theme_path()
 html_sidebars = {"**": ["searchbox.html", "localtoc.html", "globaltoc.html"]}
 html_theme_options = {
     "site_name": "Community Training Classes & Labs",
-    "next_prev_link": True,
-    "version_selector": True,
+    "next_prev_link": True
+    #"version_selector": True,
 }
 
 html_context = {
